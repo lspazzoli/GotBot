42f47e4da1e35018895286b2c163ddd20ce7a6ff
Lorenzo Spazzoli <lspazzoli@gmail.com>
2020-06-04 12:56:24 +0200

    Added Docker and started connecting FE to BE

--- GotBot/src/app/input-data/input-data.component.ts
+++ GotBot/src/app/input-data/input-data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-input-data',
   templateUrl: './input-data.component.html',
@@ -10,6 +12,28 @@ export class InputDataComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Add Name // TODO USE RESPONSE for step 2 and 3
+  addName(name) {
+    this.http.post(`api/users`, {name})
+      .subscribe(() => {
+
+      })
+  }
+
+  addEmail(email) {
+    this.http.post(`api/name`, {name})
+      .subscribe(() => {
+
+      })
+  }
+
+  addNumber(name) {
+    this.http.post(`api/mobile`, {name})
+      .subscribe(() => {
+
+      })
   }
 
 }
