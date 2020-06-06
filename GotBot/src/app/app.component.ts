d10126b85c8f7977bc122f8606282bfae0dfe764
Lorenzo Spazzoli <lspazzoli@gmail.com>
2020-06-06 21:03:38 +0200

    Fixed Anjgular input

--- GotBot/src/app/app.component.ts
+++ GotBot/src/app/app.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './input-data/input-data.component.html',
   styleUrls: ['./input-data/input-data.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'GotBot';
+  field = 0;
+  constructor(private http: HttpClient) {  }
+
+  ngOnInit(): void {
+  }
+
+  inc(){
+    this.field++;
+    alert(this.field);
+  }
+
+  nameInput(){
+    return this.field == 0;
+  }
+
+  emailInput(){
+    return this.field == 1;
+  }
+
+  cellInput(){
+      return this.field == 2;
+    }
+
+  // Add Name // TODO USE RESPONSE for step 2 and 3
+  addName(name) {
+    this.http.post(`api/users`, {name})
+      .subscribe(() => {
+
+      })
+  }
+
+  addEmail(email) {
+    this.http.post(`api/name`, {email})
+      .subscribe(() => {
+
+      })
+  }
+
+  addNumber(cell) {
+    this.http.post(`api/mobile`, {cell})
+      .subscribe(() => {
+
+      })
+  }
+
 }
--- GotBot/src/app/app.module.ts
+++ GotBot/src/app/app.module.ts
@@ -3,18 +3,17 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputDataComponent } from './input-data/input-data.component';
 import { DisplayDataComponent } from './display-data/display-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputDataComponent,
     DisplayDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
